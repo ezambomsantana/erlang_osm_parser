d7dc444cd8636398c28fe6030a7a5e43062e584a
ezambomsantana <ezambomsantana@gmail.com>
2016-10-09 23:16:16 -0300

    Update osm_to_graph.erl

--- osm_to_graph.erl
+++ osm_to_graph.erl
@@ -15,36 +15,162 @@ init(Infilename) ->
 
     ListOsm = osm_parser:show(Infilename),
     Graph = digraph:new(),
-    io:format("name: ~w~n", [ digraph:no_vertices(Graph) ]),	
+    io:format("num Vertix: ~w~n", [ digraph:no_vertices(Graph)  ]),	
+    io:format("num Edge: ~w~n", [ digraph:no_edges(Graph)  ]),	
     create_graph(ListOsm, Graph),
-    io:format("name: ~w~n", [ digraph:no_vertices(Graph) ]),	
+    io:format("num Vertix: ~w~n", [ digraph:no_vertices(Graph) ]),
+    io:format("num Edge: ~w~n", [ digraph:no_edges(Graph)  ]),	
+  %  print_node(ListOsm),	
     Graph.
 
-create_graph([Head | _List], Graph) ->
+create_graph(ListWay, Graph) ->
 
-    ListWay = element(2,Head),
+    create_vertex(Graph , ListWay),
 
-    create_vertex(Graph , ListWay).
+    create_edges(Graph , ListWay , ListWay).
 
 
 create_vertex(_Graph , []) ->
 	ok;
 
 create_vertex(Graph , [Element | MoreElements]) ->
-	io:format("passou\n"),
 	Name = element(1, Element),
-    io:format("name: ~w~n", [ Name ]),	
 	digraph:add_vertex(Graph, Name),
 	create_vertex(Graph , MoreElements).
 
-% it is missing one level
+
+
+
+% all vertex
+create_edges(_Graph , [], _List) ->
+	ok;
+
+create_edges(Graph , [Element | MoreElements], List) ->
+	NameVertex = element(1, Element),
+	NodeList = element(2, Element),
+	create_edges_vertex(Graph , NameVertex , NodeList , List),
+	create_edges(Graph , MoreElements, List).
+
+
+% all nds
+create_edges_vertex(_Graph , _NameVertex ,  [] , _List) ->
+	ok;
+
+create_edges_vertex(Graph , NameVertex , [Element | MoreElements] , List) ->
+
+	Name = element(1, Element),
+	NodeList = element(2, Element),
+
+        case Name of
+
+		nd ->
+		
+			create_edges_for_vertex(Graph , NameVertex , NodeList, List),
+			create_edges_vertex(Graph, NameVertex , MoreElements, List);
+
+		_ -> 
+
+			create_edges_vertex(Graph, NameVertex , MoreElements, List)
+
+	end.
+
+
+% all attributes
+create_edges_for_vertex(_Graph , _NameVertex , [] , _List) ->
+	ok;
+
+create_edges_for_vertex(Graph , NameVertex , [ Node | MoreNodes ] , List) ->
+
+	verify_child_destination( Graph , NameVertex , Node , List ),
+
+	create_edges_for_vertex( Graph , NameVertex , MoreNodes , List).
+
+
+
+% all nds destination
+verify_child_destination(_Graph , _NameVertex , _Node , []) ->
+	ok;
+
+verify_child_destination(Graph , NameVertex , Node , [Element | MoreElements]) ->
+
+	NameDestination = element(1, Element),
+	NodeListDestination = element(2, Element),
+
+	case NameVertex of
+
+		NameDestination -> 
+			
+			io:format("nomes iguais: ~s~n", [ NameDestination  ]),	
+			verify_child_destination( Graph , NameVertex , Node , MoreElements);
+
+		_ ->
+
+			verify_child_attribute_destination(Graph, NameVertex , NameDestination , Node , NodeListDestination),
+			verify_child_destination( Graph , NameVertex , Node , MoreElements)
+	end.
+
+	
+
+
+% all attributes_destination
+verify_child_attribute_destination(_Graph , _NameVertex , _NameDestination , _Node , []) ->
+	ok;
+
+verify_child_attribute_destination(Graph , NameVertex , NameDestination , Node , [Element | MoreElements]) ->
+
+	NodeListDestination = element(2, Element),
+
+	verify_child_origin( Graph , Node , NameVertex , NameDestination , NodeListDestination),
+
+	verify_child_attribute_destination( Graph , NameVertex , NameDestination , Node , MoreElements).
+
+
+% compare all
+verify_child_origin( _Graph , _Node , _NameVertex , _NameDestination , []) ->
+	ok;
+
+verify_child_origin( Graph , Node , NameVertex , NameDestination , [Element | MoreElements]) ->
+    IdNodeOrigin = element(2, Node),
+    IdNodeDestin = element(2, Element),
+
+    case IdNodeOrigin of
+
+	IdNodeDestin ->
+   
+   		io:format("bateu: ~s~n", [ IdNodeDestin  ]),	
+   		io:format("orgin: ~s~n", [ NameVertex  ]),
+   		io:format("destination: ~s~n", [ NameDestination  ]),
+
+		V1 = digraph:vertex(Graph, NameVertex),
+		V2 = digraph:vertex(Graph, NameDestination),
+
+   		io:format("v1: ~w~n", [ V1  ]),
+		digraph:add_edge(Graph, V1, V2),
+    		
+		true;
+
+	_ -> 
+		
+		false
+    end,
+    verify_child_origin( Graph , Node , NameVertex , NameDestination , MoreElements ).
+
+% print the list of ways and children
 print_node([]) ->
 	ok;
 
 print_node([Element | MoreElements]) ->
 	io:format("name: ~s~n", [element(1, Element)]),
-	print_attribute(element(2,Element)),
+	print_children(element(2,Element)),
 	print_node(MoreElements).
+
+print_children([]) ->
+	ok;
+
+print_children([Element | MoreElements]) ->
+	io:format("name: ~s~n", [element(1, Element)]),
+	print_attribute(element(2,Element)),
+	print_children(MoreElements).
 
 print_attribute([]) ->
 	ok;
